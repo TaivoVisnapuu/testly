19913b5d15ce7ccf670836fecb998a5b894c224a
Taivo Visnapuu <taivo.visnapuu@khk.ee>
2013-04-15 12:59:14 +0300

    just checking if git is working properly...

---
+++ classes/Request.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: taivo.visnapuu
+ * Date: 15.04.13
+ * Time: 12:49
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Request {
+	public $controller = DEFAULT_CONTROLLER;
+	public $action = 'index';
+	public $params = array();
+}

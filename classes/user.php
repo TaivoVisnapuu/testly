ae45dd092588f32c3407f61a04bae8a2d592c132
Taivo <Taivo.visnapuu@khk.ee>
2013-04-16 13:59:09 +0300

    Add controller to index.php Create user class

---
+++ classes/user.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Taivo
+ * Date: 16.04.13
+ * Time: 13:03
+ * To change this template use File | Settings | File Templates.
+ */
+
+class user {
+	public $logged_in = false;
+	function __consruct(){
+		session_start();
+		if(isset($_SESSION['user_id'])){
+			$this->logged_in = true;
+		}
+	}
+	public function require_auth(){
+		global $_REQUEST;
+		if($this->logged_in !== true){
+			if(isset($_SERVER['HTTP_X_REQUESTED_WITH'])
+			&& $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'){
+				header('HTTP/1.0 401 Unauthorized');
+				exit(json_encode(array('data' => 'session_expired')));
+			}
+			else{
+				$_SESSION['session_expired'] = true;
+				$_REQUEST->redirect('auth');
+			}
+		}
+
+	}
+}
+$_user = new user;
---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DATABASE_HOSTNAME', 'localhost');
+define('DATABASE_USERNAME', 'root');
+define('DATABASE_DATABASE', 'testly');
+
+
+
+?>

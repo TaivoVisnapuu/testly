296c7838771b3590f17544fa804ff2712c2e162c
Taivo <Taivo.visnapuu@khk.ee>
2013-04-17 11:49:48 +0300

    Add tests. Add index function. Add $requires_auth

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define('DATABASE_HOSTNAME', 'localhost');
 define('DATABASE_USERNAME', 'root');
 define('DATABASE_DATABASE', 'testly');
 define('BASE_URL', 'testly');
-define('DEFAULT_CONTROLLER', 'welcome');
+define('DEFAULT_CONTROLLER', 'tests');
 
 
 
---
+++ controllers/tests.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Taivo
+ * Date: 17.04.13
+ * Time: 11:46
+ * To change this template use File | Settings | File Templates.
+ */
+
+class tests {
+	public $requires_auth = true;
+	function index(){
+
+	}
+}
